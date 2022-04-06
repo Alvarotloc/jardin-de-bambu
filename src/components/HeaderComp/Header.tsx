345da28a9e9ab8d5b66efb069a05aae5afb04e43
Alvarotloc <alvarotloc@gmail.com>
2022-04-06 09:50:31 +0200

    Inicio de proyecto Jardin de Bambu, empezando por el header (ahora casi terminado), proximos pasos imagen decorativa y primeros textos

---
+++ src/components/HeaderComp/Header.tsx
@@ -0,0 +1,35 @@
+import React,{ useState } from "react";
+import "./Header.css";
+const Header = (): JSX.Element => {
+  const [modal, setModal] = useState<boolean>(false);
+  return (
+    <>
+      <header>
+        <nav className={modal ? "nav-izquierda activa" : "nav-izquierda"}>
+          <a href="#">MENU</a>
+          <a href="#" className="centroIzq">
+            ABOUT US
+          </a>
+          <a href="#">TODAY`S MENU</a>
+        </nav>
+        <img src="../images/Logo.png" alt="Logo de la empresa" id="logo" />
+        <nav className="nav-derecha">
+          <a href="#">
+            M <span>TAKEAWAY</span>
+          </a>
+          <a href="#" className="centroDer">
+            M <span>RESERVATIONS</span>
+          </a>
+          <a href="#">S</a>
+        </nav>
+        <div className="headerResponsive" onClick={() => setModal(!modal)}>
+          <span className={modal ? 'negativo' : ''}></span>
+          {modal ? null : <span></span>}
+          <span className={modal ? 'positivo' : ''}></span>
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default Header;
