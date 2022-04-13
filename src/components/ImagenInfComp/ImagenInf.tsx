f391843abf82d38a8e6c37dc001112a583926f71
Alvarotloc <alvarotloc@gmail.com>
2022-04-13 11:24:57 +0200

    Responsive de las imágenes y textos hecho, queda el footer y web terminada

--- src/components/ImagenInfComp/ImagenInf.tsx
+++ src/components/ImagenInfComp/ImagenInf.tsx
@@ -8,7 +8,6 @@ const ImagenInf = ({side}:side):JSX.Element => {
     return (
       <div className='contenedor-imgs'>
         <img src={side ? "../images/harina.png" : '../images/cocinero.png'} alt="Foto de cocinero" className='principal' id={side ? '' : 'derecha'}/>
-        <img src={side ? "../images/primer-plato.png" : "../images/segundo-plato.png"} alt="Foto de comida" className='secundaria' id={side ? '' : 'img-relativa'}/>
       </div>
     )
 }
